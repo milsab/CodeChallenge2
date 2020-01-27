1022e039e05efcc6918a7af19f6296d404bbab18
milsab <milad.sabouri@gmail.com>
2020-01-27 10:15:26 -0600

    String Permutation

---
+++ src/ArraysQuestions/PermutationOfString.java
@@ -0,0 +1,36 @@
+package ArraysQuestions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PermutationOfString {
+    public static void main(String[] args) {
+        for(String s : permutation("ABC")){
+            System.out.println(s);
+        }
+    }
+
+    static List<String> result = new ArrayList<>();
+    public static List<String> permutation(String s){
+        BT(s, new StringBuilder());
+        return result;
+    }
+
+    private static void BT(String s, StringBuilder cur){
+        if(cur.length() == s.length()){
+            result.add(cur.toString());
+            return;
+        }
+
+        for(int i = 0; i < s.length(); i++){
+            // choose
+            if(cur.indexOf(Character.toString(s.charAt(i))) != -1) 
+                continue;
+            cur.append(s.charAt(i));
+            // explore
+            BT(s, cur);
+            // unchoose
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+}
