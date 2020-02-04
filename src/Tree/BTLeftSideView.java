1c51f27bdb167d46aeced7f20bc79b63d241dc6e
milsab <milad.sabouri@gmail.com>
2020-02-04 17:28:09 -0600

    Binary Tree Right Side View

---
+++ src/Tree/BTLeftSideView.java
@@ -0,0 +1,34 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BTLeftSideView {
+    class TreeNode{
+        TreeNode left;
+        TreeNode right;
+        int val;
+    }
+    public List<Integer> leftView(TreeNode root){
+        if(root == null)
+            return null;
+        List<Integer> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode node = q.remove();
+                if(i == 0){
+                    ans.add(node.val);
+                }
+                q.add(node.left);
+                q.add(node.right);
+            }
+        }
+        return ans;
+    }
+}
