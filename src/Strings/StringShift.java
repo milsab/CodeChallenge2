f7aaed7ed3037a01782dadf78902fb59aba37db7
milsab <milad.sabouri@gmail.com>
2020-04-14 17:11:59 -0500

    Perform String Shifts, better solution

--- src/Strings/StringShift.java
+++ src/Strings/StringShift.java
@@ -10,9 +10,33 @@ package Strings;
 public class StringShift {
     public static void main(String[] args) {
 
+        System.out.println(stringShift("abc", new int[][]{{0,1}, {1,2}}));
         System.out.println(shift("abc", new int[][]{{0,1}, {1,2}}));
+
     }
 
+    // First Solution
+    public static String stringShift(String s, int[][] shift) {
+        int shiftLeft = 0;
+
+        for(int[] sh : shift){
+            if(sh[0] == 0)
+                shiftLeft += sh[1];
+            else
+                shiftLeft -= sh[1];
+        }
+        if(shiftLeft < 0)
+            for(int i = 0;  i < Math.abs(shiftLeft); i++){
+                s = s.substring(s.length() - 1) + s.substring(0, s.length() - 1);
+            }
+        else
+            for(int i = 0; i < shiftLeft; i++){
+                s = s.substring(1) + s.substring(0, 1);
+            }
+        return s;
+    }
+
+    // Second Solution
     public static String shift(String s, int[][] shift){
         for(int i = 0; i < shift.length; i++){
             if(shift[i][0] == 0)
