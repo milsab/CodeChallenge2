2fcca5c465e5e6a15d20f756bfa76459e02e8eac
milsab <milad.sabouri@gmail.com>
2020-04-15 14:46:49 -0500

    Product Array Except self - Better Solution

--- src/ArraysQuestions/ProductExceptSelf.java
+++ src/ArraysQuestions/ProductExceptSelf.java
@@ -4,11 +4,32 @@ package ArraysQuestions;
 // Output: [24,12,8,6]
 public class ProductExceptSelf {
     public static void main(String[] args) {
-        for(int i : product(new int[] {1,2,3,4,5})){
+        for(int i : product_2(new int[] {1,2,3,4,5})){
             System.out.printf("%d ", i);
         }
     }
-    
+    public static int[] product_2(int[] nums){
+        int len = nums.length;
+        int[] ans = new int[len];
+
+        int left = 1;
+        int right = 1;
+
+        ans[0] = left;
+        for(int i = 1; i < len; i++){
+            left *= nums[i - 1];
+            ans[i] = left;
+        }
+
+        ans[len -1] = left;
+        for(int i = len - 2; i >= 0; i--){
+            right *= nums[i + 1];
+            ans[i] *= right;
+        }
+        return ans;
+    }
+
+
     public static int[] product(int[] nums){
         int len = nums.length;
         int[] left = new int[len];
