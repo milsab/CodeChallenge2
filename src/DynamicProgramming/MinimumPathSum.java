68be4e8886a6ad8d2bd816aa008b23aba12d95df
milsab <milad.sabouri@gmail.com>
2020-04-20 17:33:21 -0500

    Min Path sum - Third Approach - Dynamic Programming

--- src/DynamicProgramming/MinimumPathSum.java
+++ src/DynamicProgramming/MinimumPathSum.java
@@ -1,4 +1,4 @@
-package DFS_BFS;
+package DynamicProgramming;
 
 public class MinimumPathSum {
 
@@ -43,5 +43,22 @@ public class MinimumPathSum {
     }
 
     // Dynamic Programming Approach
+    public static int minPathSum_3(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int[][] dp = new int[row][col];
+        dp[row - 1][col - 1] = grid[row - 1][col - 1];
+        for(int i = row - 1; i >= 0 ; i--){
+            for(int j = col - 1; j >= 0; j--){
+                if(i < row - 1 && j < col - 1)
+                    dp[i][j] = grid[i][j] + Math.min(dp[i + 1][j], dp[i][j + 1]);
+                else if( i < row - 1)
+                    dp[i][j] = grid[i][j] + dp[i + 1][j];
+                else if( j < col - 1)
+                    dp[i][j] = grid[i][j] + dp[i][j + 1];
+            }
+        }
+        return dp[0][0];
+    }
 
 }
