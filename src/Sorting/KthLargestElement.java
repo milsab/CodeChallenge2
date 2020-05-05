0b3ed3a473bb0838247ef609bd5ef8019456e84a
milsab <milad.sabouri@gmail.com>
2020-05-05 17:37:14 -0500

    Kth Largest Element in an Array - Quick Select Approach

--- src/Sorting/KthLargestElement.java
+++ src/Sorting/KthLargestElement.java
@@ -19,4 +19,46 @@ public class KthLargestElement {
 
         return minHeap.remove();
     }
+
+    // Quick Select Approach
+    public static int find_2(int[] nums, int k){
+        int targetIndex = nums.length - k;
+        return quickSelect(nums, 0, nums.length - 1, targetIndex);
+    }
+
+    public static int quickSelect(int[] nums, int left, int right, int targetIndex){
+
+        int pivot = findPivot(nums, left, right);
+
+        if(pivot == targetIndex){
+            return nums[targetIndex];
+        }
+
+        if(pivot < targetIndex){
+            return quickSelect(nums, pivot + 1, right, targetIndex);
+        } else{
+            return quickSelect(nums, left, pivot -1, targetIndex);
+        }
+
+    }
+
+    public static int findPivot(int[] nums, int left, int right){
+        int pivotIndex = left;
+        int pivot = nums[left];
+
+        for(int i = left; i < nums.length; i++){
+            if(nums[i] < pivot){
+                pivotIndex++;
+                int temp = nums[i];
+                nums[i] = nums[pivotIndex];
+                nums[pivotIndex] = temp;
+            }
+        }
+
+        int temp = nums[pivotIndex];
+        nums[pivotIndex] = nums[left];
+        nums[left] = temp;
+
+        return pivotIndex;
+    }
 }
