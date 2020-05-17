8dd6cc50595a4937610f9af9825a722fd5144221
milsab <milad.sabouri@gmail.com>
2020-05-17 14:19:49 -0500

    Sliding Window Approach

--- src/Strings/FindAllAnagrams.java
+++ src/Strings/FindAllAnagrams.java
@@ -9,8 +9,29 @@ public class FindAllAnagrams {
         System.out.print(findAnagrams("cbaebabacd", "abc"));
     }
 
+    public static  List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new ArrayList<>();
+        if (s == null || s.length() == 0 || p == null || p.length() == 0) return list;
+
+        int[] hash = new int[256]; 
+
+        for (char c : p.toCharArray()) {
+            hash[c]++;
+        }
+
+        int left = 0, right = 0, count = p.length();
+        while (right < s.length()) {
+            if (hash[s.charAt(right++)]-- >= 1) count--;
+
+            if (count == 0) list.add(left);
+
+            if (right - left == p.length() && hash[s.charAt(left++)]++ >= 0) count++;
+        }
+        return list;
+    }
+
     // Brute Force Approach
-    static public List<Integer> findAnagrams(String s, String p) {
+    static public List<Integer> findAnagrams_1(String s, String p) {
         int len = p.length();
         List<Integer> ans = new ArrayList<>();
         for(int i = 0; i <= s.length() - len; i++){
