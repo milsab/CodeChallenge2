219a04601bc5500be2843006925d6c89a3aeed51
milsab <milad.sabouri@gmail.com>
2020-05-20 17:09:45 -0500

    Binary Tree Vertical Order Traversal

--- src/Tree/VerticalOrder.java
+++ src/Tree/VerticalOrder.java
@@ -1,5 +1,29 @@
 package Tree;
 
+//Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by column).
+//
+//If two nodes are in the same row and column, the order should be from left to right.
+//
+//Examples 1:
+//
+//Input: [3,9,20,null,null,15,7]
+//
+//   3
+//  /\
+// /  \
+// 9  20
+//    /\
+//   /  \
+//  15   7
+//
+//Output:
+//
+//[
+//  [9],
+//  [3,15],
+//  [20],
+//  [7]
+//]
 import javafx.util.Pair;
 
 import java.util.*;
