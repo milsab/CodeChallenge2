9038b98881d1af065df34bb2e4848ba964f24d4d
milsab <milad.sabouri@gmail.com>
2020-06-02 12:16:44 -0500

    Level Order Traversal

---
+++ src/Tree/LevelOrderTraversal.java
@@ -0,0 +1,30 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root){
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i = 0; i < size; i++){
+                TreeNode node = q.remove();
+                level.add(node.val);
+                if(node.left != null) q.add(node.left);
+                if(node.right != null) q.add(node.right);
+            }
+            ans.add(new ArrayList<>(level));
+        }
+        return ans;
+
+    }
+}
